a5dee4aadd19893f50090d39f452c07c42c074eb
Kenneth Bradley <k_bnet570@verizon.net>
2016-07-17 08:11:17 -0400

    Added dynamic data to content-get_food_fast.php using custom fields

--- wp-content/themes/omnifoods/template-parts/content-get_food_fast.php
+++ wp-content/themes/omnifoods/template-parts/content-get_food_fast.php
@@ -1,30 +1,36 @@
 <section id="get_food_fast">
 	<div class="container">
 		<div class="section_header">
-			<i class="ion-android-restaurant section_icon"></i>
-			<h2>Get Food Fast &mdash; Not Fast Food</h2>
-			<p class="section_intro_text">Hello, were Omnifood, your new premium food delivery service. We know you&#39;re alway&#39;s busy. No time for cooking. So let us take care of that for you. We are really good at it, we promise you.</p>
+			<?php if(!empty(get_field('section_icon'))) : ?>
+				<i class="<?php echo get_field('section_icon'); ?> section_icon"></i>
+			<?php endif; ?>
+
+				<h2><?php echo get_field('section_header'); ?></h2>
+
+			<?php if(!empty(get_field('section_description'))) : ?>
+				<p class="section_intro_text"><?php echo get_field('section_description'); ?></p>
+			<?php endif; ?>
 		</div>
 		<div class="row">
 				<div class="col-sm-3">
-				<i class="ion-ios-calendar-outline icon-big" aria-hidden="true"></i>
-				<h3>Up to 365 days/year</h3>
-				<p>Never cook again! We really mean that. Our subscription plans include up to 365 days/year coverage. You can also choose to order more flexibly if that's your style.</p>
+				<i class="<?php echo get_field( 'benefit_icon1' ); ?> icon-big" aria-hidden="true"></i>
+				<h3><?php echo get_field('benefit_header1'); ?></h3>
+				<p><?php echo get_field('description1'); ?></p>
 				</div>
 				<div class="col-sm-3">
-				<i class="ion-ios-clock-outline icon-big" aria-hidden="true"></i>
-				<h3>Ready in 20 minutes</h3>
-				<p>You're only twenty minutes away from your delicious and super healthy meals delivered right to your home. We work with the best chefs in each town to ensure that you're 100% happy.</p>
+				<i class="<?php echo get_field('benefit_icon2'); ?> icon-big" aria-hidden="true"></i>
+				<h3><?php echo get_field('benefit_header2'); ?></h3>
+				<p><?php echo get_field('description2'); ?></p>
 				</div>
 				<div class="col-sm-3">
-				<i class="ion-ios-nutrition-outline icon-big" aria-hidden="true"></i>
-				<h3>100% organic</h3>
-				<p>All our vegetables are fresh, organic and local. Animals are raised without added hormones or antibiotics. Good for your health, the environment, and it also tastes better!</p>
+				<i class="<?php echo get_field('benefit_icon3'); ?> icon-big" aria-hidden="true"></i>
+				<h3><?php echo get_field('benefit_header3'); ?></h3>
+				<p><?php echo get_field('description3'); ?></p>
 				</div>
 				<div class="col-sm-3">
-				<i class="ion-ios-cart-outline icon-big" aria-hidden="true"></i>
-				<h3>Order anything</h3>
-                    <p>We don't limit your creativity, which means you can order whatever you feel like. You can also choose from our menu containing over 100 delicious meals. It's up to you!</p>
+				<i class="<?php echo get_field('benefit_icon4'); ?> icon-big" aria-hidden="true"></i>
+				<h3><?php echo get_field('benefit_header4')?></h3>
+                    <p><?php echo get_field('description4'); ?></p>
 				</div>
 		</div><!--end row-->
 	</div><!--end container-->
