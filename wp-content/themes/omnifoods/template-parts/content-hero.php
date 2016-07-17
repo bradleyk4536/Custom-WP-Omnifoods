347881a20190d1c5f0d65dd29d5d4e9be1576ec1
Kenneth Bradley <k_bnet570@verizon.net>
2016-07-17 08:23:26 -0400

    Refactored hero section by removing variables

--- wp-content/themes/omnifoods/template-parts/content-hero.php
+++ wp-content/themes/omnifoods/template-parts/content-hero.php
@@ -1,8 +1,3 @@
-<?php
-	$sub_text 		  			= get_post_meta( 11, 'sub_text', true );
-	$newsletter_text 			= get_post_meta( 11, 'newsletter_text', true);
-	$newsletter_button_text = get_post_meta( 11, 'newsletter_button_text', true);
-?>
 <section id="hero" data-type="background" data-speed="5">
 	<div class="container clear-fix">
 		<div class="row">
@@ -12,7 +7,7 @@
 			<div class="hero-text col-sm-7">
 <!-- USING GENERAL SETTING TAGLINE ENTRY -->
 			<h1><?php bloginfo( 'description' ); ?></h1>
-			<h2><?php echo $sub_text; ?></h2>
+			<h2><?php echo get_post_meta(11, 'sub_text', true) ?></h2>
 			<a href="#" class="btn btn-full">I&#39;m hungry</a>
 			<a href="#" class="btn btn-ghost">Show Me More</a>
 			</div>
@@ -24,10 +19,10 @@
 		<div class="row text-center">
 			<div class="col-sm-6">
 
-				<p class="lead"><strong><?php echo $newsletter_text; ?></strong></p>
+				<p class="lead"><strong><?php echo get_post_meta(11, 'newsletter_text', true); ?></strong></p>
 			</div>
 			<div class="col-sm-4">
-				<button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#newsletter_modal"><?php echo $newsletter_button_text; ?></button>
+				<button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#newsletter_modal"><?php echo get_post_meta(11, 'newsletter_button_text', true); ?></button>
 			</div>
 		</div>
 	</div>
