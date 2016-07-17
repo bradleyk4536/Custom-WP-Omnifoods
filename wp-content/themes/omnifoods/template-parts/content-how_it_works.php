fe7376a2ce40e6066ab92e12cea084d03243c686
Kenneth Bradley <k_bnet570@verizon.net>
2016-07-17 11:15:45 -0400

    Added custom post type ui and made how it works section dynamic

--- wp-content/themes/omnifoods/template-parts/content-how_it_works.php
+++ wp-content/themes/omnifoods/template-parts/content-how_it_works.php
@@ -1,25 +1,45 @@
 <section id="how_it_works">
 	<div class="container">
 		<div class="section_header">
-			 <h2>How it works &mdash; Simple as 1, 2, 3</h2>
+
+		 <?php if(!empty(get_field('how_works_icon'))) : ?>
+				<i class="<?php echo get_field('how_works_icon'); ?> section_icon"></i>
+		 <?php endif; ?>
+
+			 <h2><?php echo get_field('how_works_header'); ?></h2>
+
+		 <?php if(!empty(get_field('how_works_description'))) : ?>
+				<p class="section_intro_text"><?php echo get_field('how_works_description'); ?></p>
+		 <?php endif; ?>
+
 		</div>
 		<div class="row">
+
+		<?php
+			$image = get_field('how_works_image');
+			if(!empty($image)) : ?>
 			<div class="col-sm-6 phone">
-				<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/assets/images/app-iPhone.png" alt="" class="app-screen js--wp-2">
+				<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" class="app-screen js--wp-2">
 			</div>
+		<?php else :?>
+				<div class="col-sm-6 phone"></div>
+		<?php endif; ?>
+
+
+
 			<div class="col-sm-6">
+			<?php
+				$counter = 0;
+				$loop = new WP_Query( array('post_type' => 'how_works', 'orderby' => 'post_id', 'order' => 'ASC' ));
+			?>
+
+			<?php while( $loop->have_posts()) : $loop->the_post(); $counter++; ?>
 				<div class="work-steps clearfix">
-					<span>1.</span>
-					<p>Choose the subscription plan that best fits your needs and sign up today.</p>
+					<span><?php echo $counter ?></span>
+					<p><?php the_field('instructions'); ?></p>
 				</div>
-				<div class="work-steps clearfix">
-            	<span>2.</span>
-            	<p>Order your delicious meal using our mobile app or website. Or you can even call us!</p>
-                </div>
-            <div class="work-steps clearfix">
-            	<span>3.</span>
-            	<p>Enjoy your meal after less than 20 minutes. See you the next time!</p>
-            </div>
+			<?php endwhile; ?>
+
 				<a href="#" class="btn-app"><img src="<?php bloginfo( 'stylesheet_directory' )?>/assets/images/download-app-android.png" alt="Google Play"></a>
 				<a href="#" class="btn-app"><img src="<?php bloginfo( 'stylesheet_directory' ); ?>/assets/images/download-app.svg" alt="Apple Store"></a>
 			</div>
